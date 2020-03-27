67722d2c82053cc0dcc6b165fa73b784fee354a2
jsrestrepom <santyrestrepom@gmail.com>
2020-03-26 22:19:02 -0500

    Playing a little with babel and jsx.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FirstComponent, { SecondComponent } from './components/learning-examples/FirstComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
       </div>
     );
   }
---
+++ todo-app/src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function ThirdComponent() {
+    return (
+        <div className="thirdComponent">
+            ThirdComponent
+        </div>
+    );
+}
