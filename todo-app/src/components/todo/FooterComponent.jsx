5d34fea2660ed32c02e47da6a92a5585308855b0
jsrestrepom <santyrestrepom@gmail.com>
2020-07-05 12:18:08 -0500

    Implementing the API service to comunicate with the back services.

--- todo-app/src/components/todo/FooterComponent.jsx
+++ todo-app/src/components/todo/FooterComponent.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import AppService from '../../api/todo/AppService';
 
 
 class FooterComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      apiStatus: 'Unknown'
+    };
+  }
+
+  handleSuccessfulRespone(response) {
+    console.log(response);
+    this.setState({
+      apiStatus: response.data
+    });
+  }
+
+  handleFailedRespone(error) {
+    console.log(error);
+    this.setState({
+      apiStatus: 'Down'
+    });
+  }
+
   render() {
+    AppService.executeHealthCheckService()
+      .then(response => this.handleSuccessfulRespone(response))
+      .catch(error => this.handleFailedRespone(error));
     return (
       <footer className="footer">
         <span className="text-muted">Build it by @jsrestrepomoncada</span>
+        <span className="text-muted justify-content-end">API status: { this.state.apiStatus }</span>
       </footer>
     );
   }
