0bbc22838eee176f105f5dfa168e4ac459c8675d
jsrestrepom <santyrestrepom@gmail.com>
2020-07-05 18:46:11 -0500

    Refactoring the TodoData service, extracting the location constant from all the axios requests.

--- todo-app/src/api/todo/TodoDataService.js
+++ todo-app/src/api/todo/TodoDataService.js
@@ -1,31 +1,33 @@
 import axios from 'axios';
 
 
+const API_LOCATION = 'http://localhost:8080';
+
 class TodoDataService {
 
   retrieveAllTodos(username) {
     console.log('retrieveAllTodos in action');
-    return axios.get(`http://localhost:8080/users/${username}/todos`);
+    return axios.get(`${API_LOCATION}/users/${username}/todos`);
   }
 
   retrieveTodo(username, id) {
     console.log('retrieveTodo in action');
-    return axios.get(`http://localhost:8080/users/${username}/todos/${id}`);
+    return axios.get(`${API_LOCATION}/users/${username}/todos/${id}`);
   }
 
   deleteTodo(username, id) {
     console.log('deleteTodo in action');
-    return axios.delete(`http://localhost:8080/users/${username}/todos/${id}`);
+    return axios.delete(`${API_LOCATION}/users/${username}/todos/${id}`);
   }
 
   updateTodo(username, id, todo) {
     console.log('updateTodo in action');
-    return axios.put(`http://localhost:8080/users/${username}/todos/${id}`, todo);
+    return axios.put(`${API_LOCATION}/users/${username}/todos/${id}`, todo);
   }
 
   createTodo(username, todo) {
     console.log('createTodo in action');
-    return axios.post(`http://localhost:8080/users/${username}/todos`, todo);
+    return axios.post(`${API_LOCATION}/users/${username}/todos`, todo);
   }
 
 }
